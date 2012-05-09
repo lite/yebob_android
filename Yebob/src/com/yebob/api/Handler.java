d7755b1ca578069d3fb9ef96ca3e05384d590fe0
lite <litekok@gmail.com>
2012-05-09 14:48:52 +0800

    remove andengine.jar

--- Yebob/src/com/yebob/api/Handler.java
+++
@@ -1,9 +0,0 @@
-package com.yebob.api;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-public class Handler {
-	public void execute(JSONObject json) throws JSONException{};
-	public void onError(int code, String msg){};
-}
